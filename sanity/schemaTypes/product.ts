16f5fedb8302d9a1f7d679c5d4ed368f777c3005
mujtaba chandio <mujtabachandio384@gmail.com>
2025-01-21 15:34:45 +0500

    update

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -1,75 +1,52 @@
-// schemas/product.js
-
 import { defineType } from "sanity"
 
-export default defineType ({
-    name: 'product',
-    title: 'Product',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
+export default defineType({
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+      options: {
+        hotspot: true, // Enable image cropping
       },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        description: 'A unique slug for the product page',
-        options: {
-          source: 'name', // Automatically generates slug from name
-          maxLength: 96,
-        },
-      },
-      {
-        name: 'image',
-        title: 'Product Image',
-        type: 'image',
-        options: {
-          hotspot: true, // Allow the user to focus on part of the image
-        },
-        fields: [
-          {
-            name: 'alt',
-            title: 'Image Alt Text',
-            type: 'string',
-          },
-        ],
-      },
-      {
-        name: 'price',
-        title: 'Price',
-        type: 'number',
-      },
-      {
-        name: 'discountPercentage',
-        title: 'Discount Percentage',
-        type: 'number',
-        description: 'Enter discount percentage if any (e.g. 20 for 20%)',
-      },
-      {
-        name: 'isFeaturedProduct',
-        title: 'Is Featured Product',
-        type: 'boolean',
-        description: 'Check if this product is featured on the homepage',
-        initialValue: false,
-      },
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'text',
-      },
-      {
-        name: 'stockLevel',
-        title: 'Stock Level',
-        type: 'number',
-      },
-      {
-        name: 'category',
-        title: 'Category',
-        type: 'string',
-      },
-    ],
-  });
-  
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Price',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description',
+    },
+    {
+      name: 'discountPercentage',
+      type: 'number',
+      title: 'Discount Percentage',
+    },
+    {
+      name: 'isFeaturedProduct',
+      type: 'boolean',
+      title: 'Featured Product',
+    },
+    {
+      name: 'stockLevel',
+      type: 'number',
+      title: 'Stock Level',
+    },
+    {
+      name: 'category',
+      type: 'string',
+      title: 'Category',
+    },
+  ],
+});
